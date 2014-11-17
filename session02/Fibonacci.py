590ac3045e589ed52ffc526c3554d799652cc8a2
Lola Guerrero <lolaguerrero@hotmail.com>
2014-11-16 17:26:52 -0800

    I have the doc string

--- session02/Fibonacci.py
+++ session02/Fibonacci.py
@@ -6,6 +6,9 @@ Created on Sun Nov 16 16:31:22 2014
 """
 
 def fibo(n):
+    """The Fibonacci Series is a numeric series 
+    starting with the integers 0 and 1. In this series, 
+    the next integer is determined by summing the previous two"""
     if n==0:
      return 0
     elif n==1:
